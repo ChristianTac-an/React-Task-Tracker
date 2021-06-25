d492e984d22d4e5e5d2d829944c9c0935a2eac7b
ChristianTac-an <tacanchristian23@gmail.com>
2021-06-25 22:41:51 +0800

    Task Tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+import Button from './Button'
 
 
-const Header = ({title}) => {
+const Header = ({title, onAdd, showAdd}) => {
+    const location = useLocation()
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            {location.pathname === '/' && (
+            <Button 
+            color= {showAdd ? 'red' : 'green'} 
+            text={showAdd ? 'Close' : 'Add'} 
+            onClick={onAdd} />
+            )}
         </header>
     )
 }
@@ -13,7 +23,7 @@ Header.defaultProps = {
     title: 'task tracker',
 }
 
-Header.prototypes = {
+Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
